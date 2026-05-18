0c156ca72e5ef991b78ea743a53a5166f0b92213
itokagimaru <cryptomeria.rookie@gmail.com>
2026-05-18 23:14:03 +0900

    公共再生不可ワールドを設定できるように仕様追加

--- src/main/java/io/github/itokagimaru/mun10music/config/Musics.java
+++ src/main/java/io/github/itokagimaru/mun10music/config/Musics.java
@@ -1,18 +1,23 @@
 package io.github.itokagimaru.mun10music.config;
 
+import io.github.itokagimaru.mun10music.Man10Music;
 import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.List;
 
 public final class Musics {
     private final int maxLength;
     private final float defaultVolume;
     private final float autoPlayVolume;
     private final Double soundRange;
+    private final List<String> cantPlayPublicWorlds;
 
     public Musics(FileConfiguration config) {
         this.maxLength = config.getInt("musicConfig.musics.maxLength", 16384);
         this.defaultVolume = (float) config.getDouble("musicConfig.volume.default", 1.0D);
         this.autoPlayVolume = (float) config.getDouble("musicConfig.volume.autoPlay", 0.5D);
         this.soundRange = config.getDouble("musicConfig.volume.radius", 5);
+        this.cantPlayPublicWorlds = (List<String>) config.getList("musicConfig.cantPlayPublic.worldName", List.of(""));
     }
 
     public int getMaxLength() {
@@ -30,5 +35,11 @@ public final class Musics {
     public Double getSoundRange() {
         return soundRange;
     }
+
+    public List<String> getCantPlayPublicWorlds() {
+        return cantPlayPublicWorlds;
+    }
+
+
 }
 
